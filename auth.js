1189f9ce554f21b348ea1b76dce95b2811d8e60c
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-05-22 10:07:52 -0600

    Created authorization for queries

---
+++ auth.js
@@ -0,0 +1,41 @@
+// this has to be the same key used in the JWTStrategy
+const jwtSecret = 'your_jwt_secret';
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+// local passport file
+require('./passport');
+
+// used if username and password exist in database
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // username you're encoding in the JWT
+        // this specifies that token will expire in seven days
+        expiresIn: '7d',
+        // the algorithm used to sign/encode the values of JWT
+        algorithm: 'HS256',
+    });
+}
+
+// POST login
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        // ES6 shorthand for res.json({ user: user, token: token}) - keys and values are the same
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+}
