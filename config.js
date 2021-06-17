a6e500f2a6548c52504a38078d2ffcd8c600558c
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-06-17 13:28:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const Config = {
   CONNECTION_URI: process.env.CONNECTION_URI || '',
-  JWT_SECRET: process.enve.JWT_SECRET || ''
+  JWT_SECRET: process.env.JWT_SECRET || ''
 };
 
 module.exports = Config;
