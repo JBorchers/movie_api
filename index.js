9788a5eea8ca1a3785b7c861eaee8f73877f88df
JBorchers <79878454+JBorchers@users.noreply.github.com>
2021-07-01 07:49:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.get('/movies/:genres', passport.authenticate('jwt', { session: false }), (re
 // return data about genre
 app.get('movies/genres/:name', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.findOne({ 'Genre.Name' : req.params.name })
-  .then(movie => {res.status(201).json(movie.Genre);
+  .then(genre => {res.status(201).json(genre.Genre);
   })
   .catch((err) => {
     console.error(err);
